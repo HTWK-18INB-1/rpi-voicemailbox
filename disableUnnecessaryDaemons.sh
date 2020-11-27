dad06c86adefe3068b324951f25c114ea9c7fd03
Vivien Richter <vivien-richter@outlook.de>
2020-11-27 21:22:45 +0100

    added: daemons cleanup script

---
+++ disableUnnecessaryDaemons.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+sudo systemctl disable keyboard-setup.service
+sudo systemctl disable dphys-swapfile.service
+sudo systemctl disable avahi-daemon.service
+sudo systemctl disable sys-kernel-debug.mount
+sudo systemctl disable raspi-config.service
+sudo systemctl disable systemd-udev-trigger.service
+sudo systemctl disable rpi-eeprom-update.service
+sudo systemctl disable rsyslog.service
+sudo systemctl disable systemd-journald.service
+sudo systemctl disable systemd-fsck-root.service
+sudo systemctl disable systemd-logind.service
+sudo systemctl disable bluetooth.service
+sudo systemctl disable hciuart.service
+exit 0
