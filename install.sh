2fc24e024970d826ed9f8d039b80c8ff88520b41
Vivien Richter <vivien-richter@outlook.de>
2020-11-29 03:48:46 +0100

    updated: module Makefile and install script

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ sudo apt install -y espeak git bc bison flex make libssl-dev libc-dev libncurses
 ./upgrade.sh
 
 echo -e "\033[1mInstalling required packages..\033[0m"
+cd modules
 sudo make install
 
 echo -e "\033[1mInstallation finished.\033[0m"
