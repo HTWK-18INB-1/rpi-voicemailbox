fcc5b3c1d3af1d7692ae306ff2596613e5b68e66
Vivien Richter <vivien-richter@outlook.de>
2021-01-31 21:48:19 +0100

    renamed: Kernel module

--- modules/rpi_button.c
+++ modules/rpi_button.c
@@ -1,18 +1,18 @@
 /**
- * RPi voicebox driver
+ * RPi button driver
  */
 #include <linux/module.h>
 
 MODULE_AUTHOR("Vivien Richter <vivien-richter@outlook.de>");
-MODULE_DESCRIPTION("RPi voicebox driver");
+MODULE_DESCRIPTION("RPi button driver");
 MODULE_LICENSE("MIT");
 MODULE_VERSION("0.1.0");
 
 int init_module(void) {
-	pr_alert("Voicebox ready.\n");
+	pr_alert("Button ready.\n");
 	return 0;
 }
 
 void cleanup_module(void) {
-	pr_alert("Voicebox end.\n");
+	pr_alert("Button end.\n");
 }
